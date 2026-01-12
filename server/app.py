81b57597fcb44a9e8f87805a8c19d8b6a59003f6
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-13 00:10:45 +0300

    added to app.py

--- server/app.py
+++ server/app.py
@@ -7,10 +7,3 @@ from models import db
 
 # create a Flask application instance 
 app = Flask(__name__)
-
-# configure the database connection to the local file app.db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-
-# configure flag to disable modification tracking and use less memory
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
